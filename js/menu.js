2bf450288f49e5c963269b4ce661d6aa2881766a
Bill-Gates-2 <11993437@bradfordcollege.ac.uk>
2024-10-14 16:01:21 +0100

    Navbar improvements

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function menuToggle() {
+    const x = document.getElementById("menuLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
